00c5f904be4ddd0606f29a04fc99f0ff78857f1f
TheGentlemanCoder <santasmith123@hotmail.com>
2017-12-24 16:24:20 -0800

    Add skeleton structure for cracking algorithm. Still need to flesh out: - The getWordlist() method, which returns a list of words from wordlist.txt - All three of the alteration methods, whose specific algorithm has not been   agreed upon yet.

--- hashcracker.py
+++ hashcracker.py
@@ -1,9 +1,89 @@
 import hashlib
 
-def hash(string):
+def hashSHA256(string):
     return hashlib.sha256(string).hexdigest()
 
-def isHash(passwordHash, guessHash):
+def isHashMatch(passwordHash, guessHash):
     if passwordHash is guessHash:
         return True
     return False
+
+def getWordlist():
+
+def generateNextRootPassword():
+    wordlist = getWordlist()
+    rootPassword = ""
+    
+    # One word passwords
+    for first_word in wordlist:
+        rootPassword = first_word
+        yield rootPassword
+    
+    # Two word passwords
+    for second_word in wordlist:
+        for first_word in wordlist:
+            rootPassword = first_word + second_word
+            yield rootPassword
+    
+    # Three word passwords
+    for third_word in wordlist:
+        for second_word in wordlist:
+            for first_word in wordlist:
+                rootPassword = first_word + second_word + third_word
+                yield rootPassword
+    
+def applyNumberAlterationsTo(rootPassword):
+
+def applyLetterAlterationsTo(rootPassword):
+    
+def applySymbolAlterationsTo(rootPassword):
+    
+def crack(passwordHash):
+    rootPasswordGenerator = generateNextRootPassword()
+    for rootPassword in roodPasswordGenerator:
+        guessPassword = rootPassword
+        if isHashMatch(passwordHash, hashSHA256(guessPassword)):
+             return guessPassword
+                 
+                         
+        # Just number alterations
+        for numberAlteredPassword in applyNumberAlterationsTo(guessPassword):
+            if isHashMatch(passwordHash, hashSHA256(numberAlteredPassword)):
+                return numberAlteredPassword
+                     
+        # Just letter alterations
+        for letterAlteredPassword in applyLetterAlterationsTo(guessPassword):
+            if isHashMatch(passwordHash, hashSHA256(letterAlteredPassword)):
+                return letterAlteredPassword
+             
+        # Just symbol alterations
+        for symbolAlteredPassword in applySymbolAlterationsTo(guessPassword):
+            if isHashMatch(passwordHash, hashSHA256(symbolAlteredPassword)):
+                 return symbolAlteredPassword
+             
+        # Number and letter alterations
+        for numberAlteredPassword in applyNumberAlterationsTo(guessPassword):
+            for numberAndLetterAlteredPassword in applyLetterAlterationsTo(numberAlteredPassword):
+                 if isHashMatch(passwordHash, hashSHA256(numberAndLetterAlteredPassword)):
+                     return numberAndLetterAlteredPassword
+             
+        # Number and symbol alterations
+        for numberAlteredPassword in applyNumberAlterationsTo(guessPassword):
+            for numberAndSymbolAlteredPassword in applySymbolAlterationsTo(numberAlteredPassword):
+                if isHashMatch(passwordHash, hashSHA256(numberAndSymbolAlteredPassword)):
+                    return numberAndLetterSymbolPassword
+             
+        # Letter and symbol alterations
+        for letterAlteredPassword in applyLetterAlterationsTo(guessPassword):
+            for letterAndSymbolAlteredPassword in applySymbolAlterationsTo(letterAlteredPassword):
+                if isHashMatch(passwordHash, hashSHA256(letterAndSymbolAlteredPassword)):
+                    return numberAndLetterAlteredPassword
+             
+        # Number, letter, and symbol alterations
+        for numberAlteredPassword in applyNumberAlterationsTo(guessPassword):
+            for numberAndLetterAlteredPassword in applyLetterAlterationsTo(numberAlteredPassword):
+                for numberLetterAndSymbolAlteredPassword in applySymbolAlterationsTo(numberAndLetterAlteredPassword):
+                    if isHashMatch(passwordHash, hashSHA256(numberLetterAndSymbolAlteredPassword)):
+                        return numberLetterAndSymbolAlteredPassword
+                         
+    return guessPassword
